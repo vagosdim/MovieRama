ebfb2249129005889e7e5527fc87c226475d9059
vagosdim <edimoulis@cs.uoi.gr>
2024-03-31 23:00:55 +0300

    Test for reactiosn controller

--- movie_rama/spec/controllers/reactions_controller_spec.rb
+++ movie_rama/spec/controllers/reactions_controller_spec.rb
@@ -5,20 +5,37 @@ RSpec.describe ReactionsController, type: :controller do
     # Creates a valid user, and a valid movie that belongs to the user
     let (:user) {create(:user)}
     let (:movie) {create(:movie, user: user)}
-    let(:valid_attributes) { { id: movie.id, reaction_type: 'like' } }
+    
+    # Create re-usable params
+    let (:valid_attributes) {{ id: movie.id, reaction_type: ['like', 'hate'].sample }}
+    let (:invalid_attributes) {{ id: movie.id, reaction_type: 'invalid string' }}
 
     describe 'POST #react' do
         it 'When user is logged in, the movie exists and does not belong to the logged in user, and reaction type is valid' do
             sign_in user
 
-            post :react, params: { id: movie.id, reaction_type: 'like' }, format: :js#valid_attributes#{ movie_id: movie.id, reaction_type: ['like', 'hate'].sample }
+            post :react, params: valid_attributes, format: :js
             expect(response).to render_template('movies/_movie')
             expect(response).to have_http_status(:ok)
         end
+
+        it 'When user is logged in, the movie exists and does not belong to the logged in user, and reaction type is valid' do
+            sign_in user
+
+            post :react, params: invalid_attributes, format: :js
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'When user is not signed in --> unauthorized' do
+            post :react, params: valid_attributes, format: :js
+            expect(response).to have_http_status(:unauthorized)
+        end
+
     end
 
     describe 'PATCH #reverse_reaction' do
         it '' do
+            #sign_in
         end
     end
 
