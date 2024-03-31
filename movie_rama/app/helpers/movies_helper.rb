c11dd171491674e987273d384ce37d6f0f14288d
vagosdim <edimoulis@cs.uoi.gr>
2024-03-31 19:25:16 +0300

    Added Entity-Relationship Diagram, minor fixes

--- movie_rama/app/helpers/movies_helper.rb
+++ movie_rama/app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def user_display_name(movie)
+        if user_signed_in? && current_user.email == movie.user.email
+            'You'
+        else
+            movie.user.email
+        end
+    end
 end
--- movie_rama/spec/controllers/movies_controller_spec.rb
+++ movie_rama/spec/controllers/movies_controller_spec.rb
@@ -2,28 +2,26 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
     describe 'POST #create' do
-        #context 'with valid parameters' do
+        context 'with valid parameters' do
             it 'creates a new movie' do
-            user = User.create(email: 'user@movierama.com', password: 'password', password_confirmation: 'password')
-            sign_in user # Assuming you're using Devise for authentication
+                user = User.create(email: 'user@movierama.com', password: 'password', password_confirmation: 'password')
+                sign_in user # Assuming you're using Devise for authentication
 
-            post :create, params: { movie: { title: 'The Shawshank Redemption' } }
-            expect(response).to have_http_status(:redirect)
-            #expect(Movie.count).to eq(1)
-            expect(Movie.last.title).to eq('The Shawshank Redemption')
+                post :create, params: { movie: { title: 'The Shawshank Redemption' } }
+                expect(response).to have_http_status(:redirect)
+                expect(Movie.last.title).to eq('The Shawshank Redemption')
             end
-        #end
+        end
 
-        #context 'with invalid parameters' do
+        context 'with invalid parameters' do
             it 'does not create a new movie' do
-            user = User.create(email: 'user@movierama.com', password: 'password', password_confirmation: 'password')
-            sign_in user
+                user = User.create(email: 'user@movierama.com', password: 'password', password_confirmation: 'password')
+                sign_in user
 
-            post :create, params: { movie: { title: '' } }
-            expect(response).to have_http_status(:unprocessable_entity) # Assuming you render the form again on failure
-            #expect(Movie.count).to eq(0)
+                post :create, params: { movie: { title: '' } }
+                expect(response).to have_http_status(:unprocessable_entity)
             end
-        #end
+        end
     end
 
     describe 'GET #index' do
